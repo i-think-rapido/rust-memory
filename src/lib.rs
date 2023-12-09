906a7851dbf311983d9dbc5fccbb25401e60ceb1
Romeo Disca <romeo.disca@gmail.com>
2023-12-09 20:52:49 +0100

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,29 @@
+use parking_lot::RwLock;
 use std::collections::HashMap;
 use std::ops::Add;
 use std::sync::Arc;
-use parking_lot::RwLock;
-use time::OffsetDateTime;
-pub use time::Duration;
 pub use time::ext::NumericalDuration;
+pub use time::Duration;
+use time::OffsetDateTime;
+
+pub trait Memory<T> {
+    fn memoize(&self, key: &str, value: T);
+    fn retrieve(&self, key: &str) -> Option<T>;
+    fn forget(&self);
+}
+pub trait MemoryDefaultRetrieval<T>: Memory<T> {
+    fn retrieve_or_default(&self, key: &str) -> T;
+}
 
 #[derive(Clone)]
-struct Engram<T> (T, OffsetDateTime);
+struct Engram<T>(T, OffsetDateTime);
 
 #[derive(Clone)]
-pub struct Memory<T> {
+pub struct Brain<T> {
     memory: Arc<RwLock<HashMap<String, Engram<T>>>>,
     retention: Duration,
 }
-impl<T> Memory<T> {
+impl<T> Brain<T> {
     pub fn new(retention: Duration) -> Self {
         Self {
             memory: Default::default(),
@@ -22,50 +31,61 @@ impl<T> Memory<T> {
         }
     }
 }
-impl<T: Clone> Memory<T> {
-    pub fn memoize(&self, key: &str, value: T) {
-        self.memory.write().insert(key.to_string(), Engram(value, OffsetDateTime::now_utc()));
+impl<T: Clone> Memory<T> for Brain<T> {
+    fn memoize(&self, key: &str, value: T) {
+        self.memory
+            .write()
+            .insert(key.to_string(), Engram(value, OffsetDateTime::now_utc()));
     }
-    pub fn forget(&self) {
+    fn forget(&self) {
         let now = OffsetDateTime::now_utc();
         let mut binding = self.memory.write();
-        let vec = binding.iter().map(|(key, value)| (key.to_owned(), value.clone())).collect::<Vec<_>>();
+        let vec = binding
+            .iter()
+            .map(|(key, value)| (key.to_owned(), value.clone()))
+            .collect::<Vec<_>>();
         vec.iter().for_each(|(key, value)| {
             if value.1.add(self.retention) < now {
                 let _ = binding.remove(key.as_str());
             }
         });
     }
-    pub fn retrieve(&self, key: &str) -> Option<T> {
+    fn retrieve(&self, key: &str) -> Option<T> {
         self.memory.read().get(key).map(|engram| &engram.0).cloned()
     }
 }
-impl<T: Default + Clone> Memory<T> {
-    pub fn retrieve_to_value(&self, key: &str) -> T {
-        self.memory.read().get(key).map(|engram| &engram.0).cloned().unwrap_or(T::default())
+impl<T: Default + Clone> MemoryDefaultRetrieval<T> for Brain<T> {
+    fn retrieve_or_default(&self, key: &str) -> T {
+        self.retrieve(key).unwrap_or(T::default())
     }
 }
 
-pub struct Alias<'map, 'memory, T> {
+pub struct MemorySubstitute<'map, 'memory, T> {
     map: &'map HashMap<String, String>,
-    memory: &'memory Memory<T>,
+    memory: &'memory Brain<T>,
 }
-impl<'map, 'memory, T> Alias<'map, 'memory, T> {
-    pub fn new(memory: &'memory Memory<T>, map: &'map HashMap<String, String>) -> Self {
+impl<'map, 'memory, T> MemorySubstitute<'map, 'memory, T> {
+    pub fn new(memory: &'memory Brain<T>, map: &'map HashMap<String, String>) -> Self {
         Self { map, memory }
     }
 }
-impl<T: Clone> Alias<'_, '_, T> {
-    pub fn memoize(&self, key: &str, value: T) {
-        self.memory.memoize(self.map.get(key).unwrap_or(&key.to_string()), value);
+impl<T: Clone> Memory<T> for MemorySubstitute<'_, '_, T> {
+    fn memoize(&self, key: &str, value: T) {
+        self.memory
+            .memoize(self.map.get(key).unwrap_or(&key.to_string()), value);
     }
-    pub fn retrieve(&self, key: &str) -> Option<T> {
-        self.memory.retrieve(self.map.get(key).unwrap_or(&key.to_string()))
+    fn retrieve(&self, key: &str) -> Option<T> {
+        self.memory
+            .retrieve(self.map.get(key).unwrap_or(&key.to_string()))
+    }
+    fn forget(&self) {
+        self.memory.forget();
     }
 }
-impl<T: Default + Clone> Alias<'_, '_, T> {
-    pub fn retrieve_to_value(&self, key: &str) -> T {
-        self.memory.retrieve_to_value(self.map.get(key).unwrap_or(&key.to_string()))
+impl<T: Default + Clone> MemoryDefaultRetrieval<T> for MemorySubstitute<'_, '_, T> {
+    fn retrieve_or_default(&self, key: &str) -> T {
+        self.memory
+            .retrieve_or_default(self.map.get(key).unwrap_or(&key.to_string()))
     }
 }
 
@@ -76,29 +96,29 @@ mod tests {
 
     #[test]
     fn memory() {
-        let memory = Memory::new(3.milliseconds());
+        let memory = Brain::new(3.milliseconds());
 
         memory.memoize("a", 3);
         assert_eq!(memory.retrieve("a"), Some(3));
-        assert_eq!(memory.retrieve_to_value("a"), 3);
+        assert_eq!(memory.retrieve_or_default("a"), 3);
 
         std::thread::sleep(std::time::Duration::from_millis(2));
 
         memory.memoize("b", 6);
         assert_eq!(memory.retrieve("b"), Some(6));
-        assert_eq!(memory.retrieve_to_value("b"), 6);
+        assert_eq!(memory.retrieve_or_default("b"), 6);
 
         std::thread::sleep(std::time::Duration::from_millis(2));
         memory.forget();
 
         assert_eq!(memory.retrieve("a"), None);
-        assert_eq!(memory.retrieve_to_value("a"), 0);
+        assert_eq!(memory.retrieve_or_default("a"), 0);
         assert_eq!(memory.retrieve("b"), Some(6));
     }
 
     #[test]
     fn alias() {
-        let memory = Memory::new(3.milliseconds());
+        let memory = Brain::new(3.milliseconds());
 
         memory.memoize("a", 3);
         memory.memoize("b", 6);
@@ -107,7 +127,7 @@ mod tests {
             "aaa".to_string() => "a".to_string()
         );
 
-        let alias = Alias::new(&memory, &map);
+        let alias = MemorySubstitute::new(&memory, &map);
 
         assert_eq!(alias.retrieve("aaa"), Some(3));
         assert_eq!(alias.retrieve("bbb"), None);
@@ -120,5 +140,4 @@ mod tests {
         assert_eq!(alias.retrieve("ccc"), Some(9));
         assert_eq!(memory.retrieve("ccc"), Some(9));
     }
-
 }
